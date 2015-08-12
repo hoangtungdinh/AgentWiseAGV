a92b0cd1f50f8808f9fa64d5ffc744b52a66bf31
hoangtungdinh <tunghdinh@gmail.com>
2015-08-12 11:01:01 +0200

    Start develop branch

--- AgentWiseAGV/src/main/java/dmasForRouting/AGVSystem.java
+++ AgentWiseAGV/src/main/java/dmasForRouting/AGVSystem.java
@@ -1,19 +1,4 @@
 package dmasForRouting;
-/*
- * Copyright (C) 2011-2015 Rinde van Lon, iMinds-DistriNet, KU Leuven
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import static com.google.common.collect.Lists.newArrayList;
 
@@ -22,7 +7,6 @@ import java.util.Map;
 import javax.measure.unit.SI;
 
 import com.github.rinde.rinsim.core.Simulator;
-import com.github.rinde.rinsim.core.model.road.CollisionGraphRoadModel;
 import com.github.rinde.rinsim.core.model.road.RoadModelBuilders;
 import com.github.rinde.rinsim.geom.Graph;
 import com.github.rinde.rinsim.geom.Graphs;
@@ -37,11 +21,6 @@ import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Table;
 
-/**
- * Example showcasing the {@link CollisionGraphRoadModel} with an
- * {@link WarehouseRenderer} and {@link AGVRenderer}.
- * @author Rinde van Lon
- */
 public final class AGVSystem {
 
   private static final double VEHICLE_LENGTH = 2d;
