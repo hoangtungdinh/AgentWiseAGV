88c6785b3453f3fb2b90463d339626332d4e08b4
hoangtungdinh <tunghdinh@gmail.com>
2015-09-17 14:44:27 +0200

    Have bug

--- AgentWiseAGV/src/test/java/test/Test.java
+++ AgentWiseAGV/src/test/java/test/Test.java
@@ -6,9 +6,9 @@ public class Test {
 
   public static void main(String[] args) {
     final Setting setting = new Setting.SettingBuilder()
-        .setNumOfAGVs(50).setSeed(6779059).build();
+        .setNumOfAGVs(30).setSeed(7318857).build();
     // 20 9776449
-    final singlestage.delegatemas.AGVSystem agvSystem = new singlestage.delegatemas.AGVSystem(setting, true);
+    final singlestage.delegatemas.AGVSystem agvSystem = new singlestage.delegatemas.AGVSystem(setting, false);
 //    final singlestage.contextaware.AGVSystem agvSystem = new singlestage.contextaware.AGVSystem(setting, true);
     agvSystem.run();
   }
